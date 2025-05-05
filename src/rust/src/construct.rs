e80b470f3855c9013f7f9c8a871fe139cd5456eb
jdai1 <julian.dai@gmail.com>
2025-05-05 15:17:47 -0400

    temp clark-wright

--- src/rust/src/construct.rs
+++ src/rust/src/construct.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use rand::seq::SliceRandom;
 use rand::{SeedableRng, rng};
 
+use crate::common::Route;
 use crate::{common::Stop, common::VRPSolution, vrp_instance::VRPInstance};
 use std::cmp::Reverse;
 use rand::rngs::StdRng;
@@ -104,7 +105,7 @@ pub fn sweep(vrp_instance: &Arc<VRPInstance>) -> VRPSolution {
     sol
 }
 
-pub fn calculate_polar_angle(vrp_instance: &Arc<VRPInstance>, cust_no: usize) -> f64 {
+fn calculate_polar_angle(vrp_instance: &Arc<VRPInstance>, cust_no: usize) -> f64 {
     let depot_x = vrp_instance.x_coord_of_customer[0];
     let depot_y = vrp_instance.y_coord_of_customer[0];
     let cust_x = vrp_instance.x_coord_of_customer[cust_no];
@@ -114,4 +115,47 @@ pub fn calculate_polar_angle(vrp_instance: &Arc<VRPInstance>, cust_no: usize) ->
     let delta_y = depot_y - cust_y;
 
     delta_y.atan2(delta_x)
+}
+
+pub fn clarke_wright(vrp_instance: &Arc<VRPInstance>) -> VRPSolution {
+    let mut sol = VRPSolution::new(vrp_instance.clone());
+    
+    let mut routes: Vec<Route> = Vec::new();
+    for cust_no in 1..vrp_instance.num_customers {
+        let new_route = Route::new(vrp_instance.clone(), cust_no);
+        routes.push(new_route);
+    }
+
+    let mut savings_matrix: Vec<Vec<f64>> = Vec::new();
+    for i in 0..vrp_instance.num_customers {
+        let mut row = Vec::new();
+        for j in 1..vrp_instance.num_customers {
+            row.push(vrp_instance.distance_matrix.dist(i, 0) + vrp_instance.distance_matrix.dist(0, j) - vrp_instance.distance_matrix.dist(i, j));
+        }
+        savings_matrix.push(row);
+    }
+
+    loop {
+        let mut i = 0;
+        while i < routes.len() {
+            let route_a = &routes[i];
+            let route_b = &routes[i + 1];
+
+            let feasible = route_a.used_capacity() + route_b.used_capacity() <= vrp_instance.vehicle_capacity;
+            if feasible {
+                if savings_matrix[route_a.first()][route_b.last()] > 0.0 {
+                    // merge route a and b by connecting the last of route b to the first of route a
+                } else if savings_matrix[route_a.last()][route_b.first()] > 0.0 {
+                    // merge route a and b by connecting the last of route a to the first of route b
+                } else {
+                    i += 1;
+                }
+            } else {
+                i += 1;
+            }
+            
+        }
+    }
+
+    sol
 }
